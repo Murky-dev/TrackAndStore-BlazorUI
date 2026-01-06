bfe88e5937f3fce4988f8bdc19d03cd41fa574bf
Andy Freeborough <andy@freeborough.com>
2026-01-06 13:38:34 +0000

    Created entity model.
    Just doing a simple version of the enum that isn't typesafe as it's non-trivial to sort in C#!

---
+++ TrackAndStore.UI/Entity/Entity.cs
@@ -0,0 +1,18 @@
+﻿namespace TrackAndStore.UI.Entity;
+
+public static class EntityType
+{
+    public static readonly string Item = "item";
+    public static readonly string Container = "container";
+    public static readonly string Person = "person";
+    public static readonly string Location = "location";
+}
+
+public class Entity
+{
+    public int Id { get; set; }
+    public int ParentId { get; set; }
+    public string Name { get; set; } = default!;
+    public string EntityType { get; set; } = default!;
+    public string Description { get; set; } = default!;
+}
