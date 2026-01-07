bc1ac3a76b3f4db48d39ce756fe8f16c1cd463ca
Andy Freeborough <andy@freeborough.com>
2026-01-07 16:08:11 +0000

    Setup an XUnit test project.
    Apart from the obvious it also makes developing the server integration easier.

---
+++ TrackAndStore.UI.Tests/Integration/Server/EntityRestApi.cs
@@ -0,0 +1,25 @@
+﻿using TrackAndStore.UI.Entity;
+
+namespace TrackAndStore.UI.Tests.Integration.Server;
+
+public class MockHttpClientFactory : IHttpClientFactory
+{
+    public HttpClient CreateClient(string name)
+    {
+        return new HttpClient();
+    }
+}
+
+public class EntityRestApi
+{
+    [Fact]
+    public async Task GetsTheCorrectMessage()
+    {
+        var httpClientFactory = new MockHttpClientFactory();
+        var entityRepository = new EntityRepository(httpClientFactory);
+
+        var message = await entityRepository.GetMessageAsync();
+        Assert.NotNull(message);
+        Assert.Equal("Testing one two three", message);
+    }
+}
--- TrackAndStore.UI/Entity/EntityRepository.cs
+++ TrackAndStore.UI/Entity/EntityRepository.cs
@@ -4,7 +4,10 @@ public class EntityRepository(IHttpClientFactory httpClientFactory)
 {
     public async Task<string> GetMessageAsync()
     {
+        /*
         var httpClient = httpClientFactory.CreateClient();
         return await httpClient.GetStringAsync("https://www.google.com/");
+        */
+        return "Testing one two three";
     }
 }
