a17c555ef700cb56bde26e67a66cbdf22c99bbd0
Andy Freeborough <andy@freeborough.com>
2026-01-07 16:56:34 +0000

    Created first link to the backend REST API.
    - Setup basic test for each method.
    - Discovered a bug in the REST API so GetsTheCorrectEntityById is being skipped for now until that's fixed - I have raised an issue.

--- TrackAndStore.UI.Tests/Integration/Server/EntityRestApi.cs
+++ TrackAndStore.UI.Tests/Integration/Server/EntityRestApi.cs
@@ -12,14 +12,28 @@ public class MockHttpClientFactory : IHttpClientFactory
 
 public class EntityRestApi
 {
-    [Fact]
-    public async Task GetsTheCorrectMessage()
+    [Fact(Skip = "The REST API has a bug: it's returning the object wrapped in an array.")]
+    public async Task GetsTheCorrectEntityById()
     {
+        // TODO: Pull this out into properties.
         var httpClientFactory = new MockHttpClientFactory();
         var entityRepository = new EntityRepository(httpClientFactory);
 
-        var message = await entityRepository.GetMessageAsync();
-        Assert.NotNull(message);
-        Assert.Equal("Testing one two three", message);
+        var entity = await entityRepository.GetEntityByIdAsync(1);
+        Assert.NotNull(entity);
+        Assert.Equal(1, entity.Id);
+        Assert.Equal("Home", entity.Name);
+    }
+
+    [Fact]
+    public async Task GetsAllTheEntities()
+    {
+        // TODO: Pull this out into properties.
+        var httpClientFactory = new MockHttpClientFactory();
+        var entityRepository = new EntityRepository(httpClientFactory);
+
+        var entities = await entityRepository.GetEntitiesAsync();
+        Assert.Equal(15, entities.Count);
+        Assert.Equal("Hat", entities[14].Name);
     }
 }
--- TrackAndStore.UI/Entity/EntityRepository.cs
+++ TrackAndStore.UI/Entity/EntityRepository.cs
@@ -2,12 +2,22 @@
 
 public class EntityRepository(IHttpClientFactory httpClientFactory)
 {
-    public async Task<string> GetMessageAsync()
+    public async Task<Entity?> GetEntityByIdAsync(int id)
     {
-        /*
         var httpClient = httpClientFactory.CreateClient();
-        return await httpClient.GetStringAsync("https://www.google.com/");
-        */
-        return "Testing one two three";
+        // TODO: Set the base URL somewhere common (can we set this in the factory?)
+        return await httpClient.GetFromJsonAsync<Entity>($"http://localhost:3000/api/v1/entities/{id}");
+    }
+
+    public async Task<List<Entity>> GetEntitiesAsync()
+    {
+        // TODO: Pull  out into a property and/or base class.
+        var httpClient = httpClientFactory.CreateClient();
+        var entities = await httpClient.GetFromJsonAsync<List<Entity>>($"http://localhost:3000/api/v1/entities/");
+
+        if (entities == null)
+            return new List<Entity>();
+        else
+            return entities;
     }
 }
