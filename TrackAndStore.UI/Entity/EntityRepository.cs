98e72ae2412efcac819394316fc5e9f775aaba89
Andy Freeborough <andy@freeborough.com>
2026-01-08 15:22:43 +0000

    Refactored EntityRepository to make getting a HttpClient simpler.
    Just pulled it out into a private method so we don't have to keep making the factory call.

--- TrackAndStore.UI/Entity/EntityRepository.cs
+++ TrackAndStore.UI/Entity/EntityRepository.cs
@@ -2,18 +2,19 @@
 
 public class EntityRepository(IHttpClientFactory httpClientFactory)
 {
+    private HttpClient HttpClient()
+    {
+        return httpClientFactory.CreateClient("APIv1");
+    }
+
     public async Task<Entity?> GetEntityByIdAsync(int id)
     {
-        var httpClient = httpClientFactory.CreateClient("APIv1");
-        // TODO: Set the base URL somewhere common (can we set this in the factory?)
-        return await httpClient.GetFromJsonAsync<Entity>($"entities/{id}");
+        return await HttpClient().GetFromJsonAsync<Entity>($"entities/{id}");
     }
 
     public async Task<List<Entity>> GetEntitiesAsync()
     {
-        // TODO: Pull  out into a property and/or base class.
-        var httpClient = httpClientFactory.CreateClient("APIv1");
-        var entities = await httpClient.GetFromJsonAsync<List<Entity>>($"entities/");
+        var entities = await HttpClient().GetFromJsonAsync<List<Entity>>($"entities/");
 
         if (entities == null)
             return new List<Entity>();
